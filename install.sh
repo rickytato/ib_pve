ceb5b405f6e4c8a6921797c5cc5654355842a91d
rickytato <rickytato@rickytato.org>
2017-09-29 15:45:09 +0200

    Update install.sh
    Install kernel header by apt-get

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 cd /tmp
-wget http://download.proxmox.com/debian/dists/stretch/pvetest/binary-amd64/pve-headers-$(uname -v | awk '{print $4}')-pve_$(uname -v | awk '{print $4}')_amd64.deb
-dpkg -i pve-headers-$(uname -v | awk '{print $4}')-pve_$(uname -v | awk '{print $4}')_amd64.deb
+apt-get install pve-headers-$(uname -v | awk '{print $4}')-pve
 apt-get update
 apt-get -y install python-libxml2 make dkms
 wget http://content.mellanox.com/ofed/MLNX_OFED-4.1-1.0.2.0/MLNX_OFED_LINUX-4.1-1.0.2.0-debian8.7-x86_64.iso
